c0b5995c72a6c731fbbbbfa4462b322b80337f8d
chae1405 <chae1405@nate.com>
2021-08-21 18:51:07 +0900

    aa

---
+++ Node.java
@@ -0,0 +1,24 @@
+public class Node<T>
+//2.1노드 클래스 구현
+{
+//    //여기 안에는 데이터가 있고
+//    T data; //
+//    Node<T> next = null;// 다음 노드(객체)를 가리키는 포인터
+//
+//    public Node(T data)//생성자는
+//    {
+//        this.data = data;// 데이터로만 받아서 저장 하면 된다.
+//    }
+//    //2.2 노드와 노드 연결하기 : 노드 인스턴스간 연결
+//    Node<Integer> node1 = new Node<Integer>(1);
+//    Node<Integer> node2 = new Node<Integer>(2);
+//    //node1.next = node2; //에러뜸?? 왜지??
+//    Node head = node1; // 노드 헤드 선언
+// 링크드 리스트에 데이터 추가하기기
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
